c2ff8b17dffd91fdb6692c6efa211add77cce2c5
Igor Sasaki <igor-sasaki@hotmail.com>
2021-07-29 23:38:37 -0300

    style(libraries assets styles): implementa styled-components, estilo global e assets da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 // External Modules
 import React from "react";
 
+// Styling
+import { GlobalStyle } from "./styles/global";
+
 export const App: React.FC = () => {
   return (
-    <div>
+    <div className="App">
       <h1>Hello World</h1>
+
+      <GlobalStyle />
     </div>
   );
 };
---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+// External Modules
+import { createGlobalStyle } from "styled-components";
+
+// Libraries
+import { Colors } from "../libraries/Color";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: ${Colors.background};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body,input,textarea,button {
+    font-family: 'Poppins', sans-serif;
+    font-weight:400;
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled]{
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
